18d40f2699c1777d8a419d782336de376c63d494
tvo93 <tvo93@uw.edu>
2018-02-14 21:44:00 -0800

    Initial commit

---
+++ VolunteerSystem/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VolunteerSystem.DAL;
+using VolunteerSystem.ViewModels;
+namespace VolunteerSystem.Controllers
+{
+   
+
+
+    public class HomeController : Controller
+
+    {
+        private CompanyContext db = new CompanyContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from volunteer in db.Volunteers
+                                                 
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                      // EnrollmentDate = dateGroup.Key,
+                                                      // VolunteerCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VolunteerSystem/DAL/CompanyContext.cs
@@ -0,0 +1,23 @@
+﻿using VolunteerSystem.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace VolunteerSystem.DAL
+{
+  
+        public class CompanyContext : DbContext
+        {
+
+            public CompanyContext() : base("CompanyContext")
+            {
+            }
+
+            public DbSet<Volunteer> Volunteers { get; set; }
+            public DbSet<Enrollment> Enrollments { get; set; }
+            public DbSet<Job> Jobs { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            }
+        }
+}
